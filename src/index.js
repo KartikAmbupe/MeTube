65bd76f8ced3d49173b9ec1823f5688681e7aa90
KartikAmbupe <kartikambupe@gmail.com>
2025-02-02 15:05:28 +0530

    mongo-atlas-connection

---
+++ src/index.js
@@ -0,0 +1,38 @@
+// require('dotenv').config({path: './env'}) //we could import dotenv this way but we are following module approach(ES6)
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./env",
+})
+
+
+
+connectDB();
+
+
+
+// One of the ways to connect to database
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants.js";
+/*
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("errror", () => {
+        console.log("ERRRRRROR: ", error);
+        throw error;
+    })
+    app.listen(process.env.PORT, () => {
+        console.log(`App is listening on port ${process.env.PORT}`);
+    })
+  } catch (error) {
+    console.error("ERROR: ", error);
+    throw err;
+  }
+})();
+*/ 
