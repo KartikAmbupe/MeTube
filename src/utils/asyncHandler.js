241002e2d0dce8e32faf47cb93d9c9cb44609cb8
KartikAmbupe <kartikambupe@gmail.com>
2025-02-03 20:33:39 +0530

    utils

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+//This version passes the error to the next middleware
+
+export {asyncHandler}
+
+//this version sends a JSON response with error message and status code
+// wrapper function using async/await
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
