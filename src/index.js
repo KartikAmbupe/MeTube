01bba5893578bf629b56cce3d90221789b701a1c
KartikAmbupe <kartikambupe@gmail.com>
2025-02-04 23:55:42 +0530

    models

--- src/index.js
+++ src/index.js
@@ -4,22 +4,18 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: "./env",
-})
-
-
+  path: "./env",
+});
 
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
       console.log(` Server is running at port: ${process.env.PORT}`);
-    })
+    });
   })
   .catch((err) => {
     console.log("MongoDB connection failed :(", err);
-  })
-
-
+  });
 
 // One of the ways to connect to database
 // import mongoose from "mongoose";
@@ -43,4 +39,4 @@ const app = express();
     throw err;
   }
 })();
-*/ 
+*/
