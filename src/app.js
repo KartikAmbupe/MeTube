e56065b8594d19dfe1b29a8643ca69c9647e07de
KartikAmbupe <kartikambupe@gmail.com>
2025-02-13 23:23:08 +0530

    setup-config

--- src/app.js
+++ src/app.js
@@ -34,6 +34,6 @@ import userRouter from './routes/user.routes.js'
 
 //routes declaration
 app.use("/api/v1/users", userRouter);
-// https://localhost:8000/api/v1/users/register
+//the url will be: https://localhost:8000/api/v1/users/register
 
 export { app };
