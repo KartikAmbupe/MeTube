320a12e09a95e039d3d7cb054370dd9bf9088db9
KartikAmbupe <kartikambupe@gmail.com>
2025-02-18 20:28:21 +0530

    add register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
+import { cloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   // res.status(200).json({
@@ -40,7 +42,39 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(409, "User with same username or email already exists");
   }
 
-  req.files?.avator[0]?.path
+  const avatarLocalPath = req.files?.avatar[0]?.path; //? is optional chaining operator, 
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if(!avatarLocalPath){
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if(!avatar){
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  //creating entry in db
+  User.create({
+    fullname,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase()
+  })
+
+  const createdUser = await User.findById(user._id).select("-password -refreshToken ")// password and refreshtoken fields will be excluded from the returned doc
+
+  if(!createdUser){
+    throw new ApiError(500, "Something went wrong while registering the user");
+  }
+
+  return res.status(201).json(
+    new ApiResponse(200, createdUser, "User registered successfully")
+  )
 });
 
 export {registerUser};
